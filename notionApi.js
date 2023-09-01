5f637ab3e676d1633eb4e357a0876489454d48ea
SharkniA <zel@kakao.com>
2023-09-01 14:52:04 +0000

    typescript 적용

--- notionApi.js
+++ notionApi.js
@@ -1,44 +1,114 @@
-require("dotenv").config();
-const { Client } = require("@notionhq/client");
-
-const pageId = process.env.NOTION_PAGE_ID;
-const key = process.env.NOTION_KEY;
-
-const notion = new Client({ auth: key });
-
-(async () => {
-    try {
-        const response = await notion.databases.query({ database_id: pageId });
-        // console.log(response);
-        //페이지들을 돌면서 id로 페이지를 읽어온다. 
-        response.results.forEach(page => {
-            (async () => {
-                const pageId = page.id;
-                //page properties
-                const response = await notion.pages.retrieve({ page_id: pageId });
-                console.log("----------Page Properties----------")
-                console.log(JSON.stringify(response, null, 2));
-                //page contents
-                (async () => {
-                    const blockId = pageId;
-                    const response = await notion.blocks.retrieve({
-                        block_id: blockId,
-                    });
-                    console.log("----------Page Contents as Block----------")
-                    console.log(JSON.stringify(response, null, 2))
-                })();
-                (async () => {
-                    const blockId = pageId;
-                    const response = await notion.blocks.children.list({
-                        block_id: blockId,
-                        page_size: 50,
-                    });
-                    console.log("----------Page Block List----------");
-                    console.log(JSON.stringify(response, null, 2));
-                })();
-            })();
-        });
-    } catch (error) {
-        console.error("Error querying the database:", error);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-})();
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv = require("dotenv");
+var client_1 = require("@notionhq/client");
+dotenv.config();
+var pageId = process.env.NOTION_PAGE_ID;
+var key = process.env.NOTION_KEY;
+var notion = new client_1.Client({ auth: key });
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    var response, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, notion.databases.query({ database_id: pageId })];
+            case 1:
+                response = _a.sent();
+                response.results.forEach(function (page) {
+                    (function () { return __awaiter(void 0, void 0, void 0, function () {
+                        var pageId, pageResponse;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0:
+                                    pageId = page.id;
+                                    return [4 /*yield*/, notion.pages.retrieve({ page_id: pageId })];
+                                case 1:
+                                    pageResponse = _a.sent();
+                                    console.log("----------Page Properties----------");
+                                    console.log(JSON.stringify(pageResponse, null, 2));
+                                    (function () { return __awaiter(void 0, void 0, void 0, function () {
+                                        var blockId, blockResponse;
+                                        return __generator(this, function (_a) {
+                                            switch (_a.label) {
+                                                case 0:
+                                                    blockId = pageId;
+                                                    return [4 /*yield*/, notion.blocks.retrieve({
+                                                            block_id: blockId,
+                                                        })];
+                                                case 1:
+                                                    blockResponse = _a.sent();
+                                                    console.log("----------Page Contents as Block----------");
+                                                    console.log(JSON.stringify(blockResponse, null, 2));
+                                                    return [2 /*return*/];
+                                            }
+                                        });
+                                    }); })();
+                                    (function () { return __awaiter(void 0, void 0, void 0, function () {
+                                        var blockId, childrenListResponse;
+                                        return __generator(this, function (_a) {
+                                            switch (_a.label) {
+                                                case 0:
+                                                    blockId = pageId;
+                                                    return [4 /*yield*/, notion.blocks.children.list({
+                                                            block_id: blockId,
+                                                            page_size: 50,
+                                                        })];
+                                                case 1:
+                                                    childrenListResponse = _a.sent();
+                                                    console.log("----------Page Block List----------");
+                                                    console.log(JSON.stringify(childrenListResponse, null, 2));
+                                                    return [2 /*return*/];
+                                            }
+                                        });
+                                    }); })();
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); })();
+                });
+                return [3 /*break*/, 3];
+            case 2:
+                error_1 = _a.sent();
+                console.error("Error querying the database:", error_1);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); })();

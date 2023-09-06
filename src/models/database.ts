330cac880345b60bd4d3f9e787786d55c8f172d0
Sharknia <53037836+Sharknia@users.noreply.github.com>
2023-09-06 22:41:14 +0900

    utils 분리, envConfig class 생성

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client";
 import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
-import * as dotenv from "dotenv";
-import { NotionAPI } from "./notionapi";
+import { NotionAPI } from "../utils/notionapi";
+import { EnvConfig } from "../utils/envConfig";
 
 export class DataBase {
     private static instance: DataBase | null = null;
@@ -20,9 +20,9 @@ export class DataBase {
 
     public static async create(filterUdate?: string): Promise<DataBase> {
         if (!this.instance) {
-            dotenv.config({ path: `${__dirname}/../../.env` });
-            const notionkey: string = process.env.NOTION_KEY || "";
-            const databaseid: string = process.env.NOTION_DATABASE_ID || "";
+            const envConfig: EnvConfig = EnvConfig.create();
+            const notionkey: string = envConfig.notionKey || "";
+            const databaseid: string = envConfig.databaseid || "";
 
             if (!notionkey || !databaseid) {
                 throw new Error("NOTION_KEY or NOTION_DATABASE_ID is missing in the environment variables.");
---
+++ src/utils/envConfig.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+
+export class EnvConfig {
+    private static instance: EnvConfig | null = null;
+
+    public notionKey: string | null = null;
+    public databaseid: string | null = null;
+
+    private constructor() {
+        dotenv.config({ path: `${__dirname}/../../.env` });
+        this.notionKey = process.env.NOTION_KEY || "";
+        this.notionKey = process.env.NOTION_DATABASE_ID || "";
+    }
+
+    public static create() {
+        if (!this.instance) {
+            this.instance = new EnvConfig();
+        }
+        return this.instance;
+    }
+}

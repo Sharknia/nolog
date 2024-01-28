fa66ed32a737956992dc0c8312d2e4e9fe529bc7
Sharknia <53037836+Sharknia@users.noreply.github.com>
2024-01-28 18:31:17 +0900

    formatTextElement로 반복 로직 분리

--- src/utils/markdownConverter.ts
+++ src/utils/markdownConverter.ts
@@ -29,7 +29,13 @@ export class MarkdownConverter {
                 markdown += this.convertParagraph(block.paragraph);
                 break;
             case 'heading_1':
+                markdown += this.convertHeading(block.heading_1, 1);
                 break;
+            case 'heading_2':
+                markdown += this.convertHeading(block.heading_2, 2);
+                break;
+            case 'heading_3':
+                markdown += this.convertHeading(block.heading_3, 3);
             // 다른 블록 유형에 대한 처리를 여기에 추가...
             default:
                 console.warn(
@@ -41,38 +47,47 @@ export class MarkdownConverter {
 
     private convertParagraph(paragraph: any): string {
         let markdown = '';
-
         for (const textElement of paragraph.rich_text) {
-            let textContent = textElement.plain_text;
-
-            // 텍스트 스타일링 처리
-            if (textElement.annotations.bold) {
-                textContent = `**${textContent}**`;
-            }
-            if (textElement.annotations.italic) {
-                textContent = `*${textContent}*`;
-            }
-            if (textElement.annotations.strikethrough) {
-                textContent = `~~${textContent}~~`;
-            }
-            if (textElement.annotations.code) {
-                textContent = `\`${textContent}\``;
-            }
-            if (textElement.annotations.underline) {
-                // 마크다운은 기본적으로 밑줄을 지원하지 않으므로, HTML 태그 사용
-                textContent = `<u>${textContent}</u>`;
-            }
-            // 색상 처리 (HTML 스타일을 사용)
-            if (textElement.annotations.color !== 'default') {
-                textContent = `<span style="color: ${textElement.annotations.color};">${textContent}</span>`;
-            }
-            if (textElement.href) {
-                textContent = `[${textContent}](${textElement.href})`;
-            }
-            markdown += textContent;
+            markdown += this.formatTextElement(textElement);
         }
-        markdown = markdown + '\n\n';
-        console.log(`convertParagraph - markdown : ${markdown}`);
-        return markdown;
+        return markdown + '\n\n';
+    }
+
+    private convertHeading(heading: any, level: number): string {
+        let markdown = '';
+        const prefix = '#'.repeat(level + 1) + ' ';
+        for (const textElement of heading.rich_text) {
+            markdown += this.formatTextElement(textElement);
+        }
+        return prefix + markdown + '\n\n';
+    }
+
+    private formatTextElement(textElement: any): string {
+        let textContent = textElement.plain_text;
+
+        // 텍스트 스타일링 처리
+        if (textElement.annotations.bold) {
+            textContent = `**${textContent}**`;
+        }
+        if (textElement.annotations.italic) {
+            textContent = `*${textContent}*`;
+        }
+        if (textElement.annotations.strikethrough) {
+            textContent = `~~${textContent}~~`;
+        }
+        if (textElement.annotations.code) {
+            textContent = `\`${textContent}\``;
+        }
+        if (textElement.annotations.underline) {
+            textContent = `<u>${textContent}</u>`;
+        }
+        if (textElement.annotations.color !== 'default') {
+            textContent = `<span style="color: ${textElement.annotations.color};">${textContent}</span>`;
+        }
+        if (textElement.href) {
+            textContent = `[${textContent}](${textElement.href})`;
+        }
+
+        return textContent;
     }
 }

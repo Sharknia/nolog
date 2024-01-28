aa74bc51db20375121b9618fb7c47f42ee5cc1f9
Sharknia <53037836+Sharknia@users.noreply.github.com>
2024-01-28 16:42:33 +0900

    block.ts 유니온 타입 문제 해결

--- src/models/block.ts
+++ src/models/block.ts
@@ -1,5 +1,8 @@
 import { Client } from '@notionhq/client';
-import { BlockObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import {
+    BlockObjectResponse,
+    PartialBlockObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 import { GetBlockResponse } from '@notionhq/client/build/src/api-endpoints';
 
 export class Block {
@@ -21,28 +24,30 @@ export class Block {
         return await this.notion.blocks.retrieve({ block_id: this.blockId });
     }
 
-    private async processBlock(block: BlockObjectResponse): Promise<string> {
+    private async processBlock(block: GetBlockResponse): Promise<string> {
         let markdown = '';
-
-        switch (block.type) {
-            case 'paragraph':
-                markdown += this.convertParagraph(block.paragraph);
-                break;
-            case 'heading_1':
-                markdown += `# ${block.heading_1.rich_text
-                    .map((t) => t.plain_text)
-                    .join('')}\n\n`;
-                break;
-            // 다른 블록 유형에 대한 처리를 여기에 추가...
-            default:
-                console.warn(`Unsupported block type: ${block.type}`);
+        // BlockObjectResponse 인 경우
+        if ('type' in block) {
+            switch (block.type) {
+                case 'paragraph':
+                    markdown += this.convertParagraph(block.paragraph);
+                    break;
+                case 'heading_1':
+                    markdown += `# ${block.heading_1.rich_text
+                        .map((t) => t.plain_text)
+                        .join('')}\n\n`;
+                    break;
+                // 다른 블록 유형에 대한 처리를 여기에 추가...
+                default:
+                    console.warn(`Unsupported block type: ${block.type}`);
+            }
+            // 하위 블록 처리 (재귀적)
+            if (block.has_children) {
+                markdown += await this.processChildBlocks(block.id);
+            }
+        } else {
+            // PartialBlockObjectResponse인 경우.. 이건 뭘까?
         }
-
-        // 하위 블록 처리 (재귀적)
-        if (block.has_children) {
-            markdown += await this.processChildBlocks(block.id);
-        }
-
         return markdown;
     }
 

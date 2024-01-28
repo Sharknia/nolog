efcb9fe2830e44f81df8e1e95fd42bf0c98d1cda
Sharknia <53037836+Sharknia@users.noreply.github.com>
2024-01-28 19:19:39 +0900

    설정 파일에 블로그 주소 추가

--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -1,15 +1,17 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 export class EnvConfig {
     private static instance: EnvConfig | null = null;
 
     public notionKey: string | null = null;
     public databaseid: string | null = null;
+    public blogUrl: string | null = null;
 
     private constructor() {
         dotenv.config({ path: `${__dirname}/../../.env` });
-        this.notionKey = process.env.NOTION_KEY || "";
-        this.databaseid = process.env.NOTION_DATABASE_ID || "";
+        this.notionKey = process.env.NOTION_KEY || '';
+        this.databaseid = process.env.NOTION_DATABASE_ID || '';
+        this.blogUrl = process.env.BLOG_URL || '';
     }
 
     public static create() {

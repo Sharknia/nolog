b1cb802d195a5f04b4122398f41bc539980aee97
Sharknia <53037836+Sharknia@users.noreply.github.com>
2024-02-03 00:03:53 +0900

    열거형 정의 완료

--- src/models/enums.ts
+++ src/models/enums.ts
@@ -1,5 +1,8 @@
 // 페이지 상태값 열거형 정의
 export enum PageStatus {
-    Deleted = 'deleted',
-    Writing = 'writing',
+    Deleted = 'Deleted', // 삭제됨
+    Writing = 'Writing', // 작성중
+    Ready = 'Ready', // 포스팅 준비됨
+    Updated = 'Updated', // 업데이트됨
+    ToBoDeleted = 'ToBoDeleted', // 삭제될 예정
 }
